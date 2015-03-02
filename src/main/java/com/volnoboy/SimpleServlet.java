f48f009943f76e3384c0016796c724dbccfdb78c
volnoboy <volnoboy@gmail.com>
2015-03-02 19:48:00 +0200

    Error page

--- src/main/java/com/volnoboy/SimpleServlet.java
+++ src/main/java/com/volnoboy/SimpleServlet.java
@@ -2,6 +2,7 @@ package com.volnoboy;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,7 +23,7 @@ public class SimpleServlet extends HttpServlet {
 	}
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String name  = request.getParameter("name");
 		if (name != null) {
 			response.setContentType("text/xml");
@@ -32,7 +33,7 @@ public class SimpleServlet extends HttpServlet {
 					"<product>%s</product>" +
 					"</application>", name, appName);
 		} else {
-			response.getWriter().write("Please enter name value!");
+			throw new ServletException("A name shoul be entered");
 		}
 	}
 
---
+++ src/main/webapp/error.jsp
@@ -0,0 +1,11 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" isErrorPage="true" %>
+<html>
+<head>
+    <title>Error Page!</title>
+</head>
+<body>
+<h2>Error page!!!</h2>
+<%= exception.getMessage()%>
+</body>
+</html>
