70cc81062744e7e01bfc61b156cfaf6a7b1cc525
volnoboy <volnoboy@gmail.com>
2015-03-02 13:05:58 +0200

    Servlet init

--- src/main/java/com/volnoboy/SimpleServlet.java
+++ src/main/java/com/volnoboy/SimpleServlet.java
@@ -1,27 +1,39 @@
 package com.volnoboy;
 
+import java.io.IOException;
+
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
 * Created by Reuven on 2/28/15.
 */
-@WebServlet(urlPatterns = {"/home", "/myhome", "*.web"})
-public class OurServlet extends HttpServlet {
+@WebServlet(urlPatterns = {"/home", "*.do"}, name="SimpleServlet", initParams = {@WebInitParam(name = "ProductName", value = "Welcome Application")})
+public class SimpleServlet extends HttpServlet {
+	String appName = "My application";
+
+	@Override
+	public void init() {
+		appName = getInitParameter("ProductName");
+		appName = getServletContext().getInitParameter("ProductName");
+	}
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String name  = request.getParameter("name");
 		if (name != null) {
 			response.setContentType("text/xml");
-			response.getWriter().printf("<name>Hello %s</name>", name);
+			response.getWriter().printf(
+					"<application>" +
+					"<name>Hello %s</name>" +
+					"<product>%s</product>" +
+					"</application>", name, appName);
 		} else {
 			response.getWriter().write("Please enter name value!");
 		}
-		//response.getWriter().write("Zelexon");
 	}
 
 	@Override
@@ -33,6 +45,5 @@ public class OurServlet extends HttpServlet {
 			response.getWriter().write("Please enter name value!");
 			response.sendRedirect("index.jsp");
 		}
-		//response.getWriter().write("Zelexon");
 	}
 }
